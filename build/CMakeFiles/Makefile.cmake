9545848eec00f65ede077f20a43cf96c6f7388e1
Yuqing Wang <yuqingwang@YuqingdeMacBook-Pro.local>
2022-09-13 22:39:31 +0800

    [*] test12_assimp

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -47,5 +47,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test11.dir/DependInfo.cmake"
+  "CMakeFiles/test12.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/test12.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test12.dir/test12_assimp.cpp.o"
+  "CMakeFiles/test12.dir/glad.c.o"
+  "test12.pdb"
+  "test12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
